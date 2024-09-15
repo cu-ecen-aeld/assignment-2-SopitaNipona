cfa09292ad26d28ecb6a0d221e263c02f5351970
SopitaNipona <a01748692@tec.mx>
2024-09-14 20:54:32 -0600

    1 delivery

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -3,9 +3,18 @@
 filesdir=$1
 searchstr=$2
 
-if [ -Z "$filesdir" ];
+if [ -d "$filesdir" ] && [ ! -z "$searchstr" ];
 then
-  exit 1
+  files=$(ls -1 | wc -l)
+  lines=$(grep -r "$filesdir" "$searchstr" | wc -l)
+  if [ $? -eq 0 ];
+  then
+    echo "The number of files are ${files} and the number of matching lines are ${lines}"
+  fi
+
+  exit 0
 else
 
+
+  exit 1
 fi
